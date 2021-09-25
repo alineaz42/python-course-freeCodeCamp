237eab6d8a17a38320fc30f372ffcdd4fe332508
Ali Neaz <neazali77@gmail.com>
2021-09-26 01:54:47 +0600

    video 1:28min done

--- chapter three/if.py
+++ chapter three/if.py
@@ -1,6 +1,18 @@
-x = 5
-if x < 5:
-    print("smalller")
+# x = 5
+# if x < 5:
+#     print("smalller")
+# else:
+#     print("bigger")
+# print("finish")
+
+a = 45
+if a < 20:
+    if a < 10:
+        print("No")
+elif a > 20:
+    if a < 30:
+        print("Yes")
+    else:
+        print("yes home baby")
 else:
-    print("bigger")
-print("finish")
+    print("No way home baby")
